1f99de9f73dfbb88e3b8bd0a8820066649da9b17
Rafael <maciel.rafael1@gmail.com>
2023-04-20 10:41:59 -0400

    complete remaining sections

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
 import LineGradient from "./components/LineGradient";
 import MySkills from "./scenes/MySkills";
+import Projects from "./scenes/Projects";
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -41,6 +44,15 @@ function App() {
       <div className="w-5/6 mx-auto md:h-full">
         <MySkills />
       </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <Projects />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <Contact />
+      </div>
+      <Footer />
     </div>
   );
 }

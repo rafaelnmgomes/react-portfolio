0f123f039f86664b05ab8e725c35dae60ea97fed
Rafael <maciel.rafael1@gmail.com>
2023-05-11 15:15:19 -0400

    rollback

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <>
-      <App />
-      <Analytics />
-    </>
+    <App />
+    <Analytics />
   </React.StrictMode>
 );

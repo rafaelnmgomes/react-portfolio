7030b1cc4004d5639d9c2e6095dc0a2284ed8f2b
Rafael <maciel.rafael1@gmail.com>
2023-05-11 15:14:58 -0400

    redeploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-    <Analytics />
+    <>
+      <App />
+      <Analytics />
+    </>
   </React.StrictMode>
 );
